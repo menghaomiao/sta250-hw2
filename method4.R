677afd37757da9ae9d31e74f97bb1d601b1b4dd8
Walter <haomiaomeng@126.com>
2014-02-13 16:40:04 -0800

    modified method4 and submit hadoop methods

--- method4.R
+++ method4.R
@@ -3,9 +3,17 @@
 #Usage: method4(cores), where cores is the number of help nodes.
 
 method4=function(cores=32) {
- f=sapply(files, list)
- t=getDelayTable_thread(f, ind2-1, cores)
- arrdelay=as.integer(names(t))
- freq=as.integer(t)
+ t=data.frame(0, 0)
+ cl=makeCluster(cores, 'FORK')
+ nt=clusterApplyLB(cl, 1:81, function(i) as.data.frame(as.table(getDelayTable(files[i]))))
+ stopCluster(cl)
+ for (i in 1:81) {
+  t=merge(t, nt[[i]], by.x=names(t)[1], by.y=names(nt[[i]])[1], all=T, sort=F)
+  t[is.na(t)]=0
+  t[2]=t[2]+t[3]
+  t[3]=NULL
+ }
+ arrdelay=as.integer(t[[1]])
+ freq=t[[2]]
  arrstat(arrdelay, freq)
 }
